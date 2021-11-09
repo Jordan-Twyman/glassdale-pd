9e13e627e69b200f495b06dce8ee302c22b46701
Jaytwy24 <jtwy24@gmail.com>
2021-11-09 15:57:18 -0500

    updated note card

--- src/scripts/notes/Note.js
+++ src/scripts/notes/Note.js
@@ -27,7 +27,7 @@ import { NoteEditForm } from "./NoteEditForm.js";
 export const Note = (notes) => {
     return `
       <section class="note-card">
-        <h3 class="note__suspect">${notes.suspect}</h3>
+        <h3 class="note__suspect">Suspect: ${notes.suspect}</h3>
         <p class="note__text">${notes.text}</p>
         <p class="note__date"><b>Date: </b> ${notes.date}</p>
         <section class="note__buttons">
